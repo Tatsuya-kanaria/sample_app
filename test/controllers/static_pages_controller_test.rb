62f7d05a3d7fb18d882470d1aa105f8211384c75
Tatsuya-kanaria <kanaria.git@gmail.com>
2020-09-21 10:02:10 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -29,4 +29,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
+
+  test "sould get root" do
+    get root_url
+    assert_response :success
+  end
 end
